0e9d2a3b724dd540468922eb9b0e7d9ee354d91e
catcat520 <catcat520@users.noreply.github.com>
2017-02-22 14:44:45 +0800

    Update asc.cmd

--- AmazingSmartCalc/asc.cmd
+++ AmazingSmartCalc/asc.cmd
@@ -1,8 +1,12 @@
 @echo off
+
+:Begin
 if "%1"=="" (
 cls
-set tips=AmazingSmartCalc 2017.02.21.1.3^^(Base on GnuWin32bc^^^)
+set tips=AmazingSmartCalc 2017.02.22.1.4^^(Base on GnuWin32bc^^^)
 title %tips%
 echo %tips%
 )
+
+:Main
 if not "%1"=="" E:\PathTools\gnuwin32\bc\catcat520.gnuwin32.echo.exe %*|E:\PathTools\gnuwin32\bc\catcat520.gnuwin32.bc.exe -l
